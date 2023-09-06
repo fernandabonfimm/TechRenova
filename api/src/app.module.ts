8cce513244de68f8400e684d1025a95f7f462ae2
Pedro <pedroubine@proton.me>
2023-09-06 20:33:12 -0300

    stufff

---
+++ api/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+@Module({
+  imports: [UserModule, AuthModule],
+  controllers: [AppController, AuthController],
+  providers: [AppService],
+})
+export class AppModule {}
